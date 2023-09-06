5521f2cf6138abfb9b9377383e0e9ba40be749c5
Joshua Ashton <joshua@froggi.es>
2023-09-06 22:00:26 +0100

    drm/amd/display: Don't consider vblank passed if currently in vertical front porch time
    Changing refresh rates on OLED displays works differently to typical
    LCD panels in that instead of changing the clock, the vertical porch
    is extended significantly for lower rates.
    
    This can mean that the vertical porch can be incredibly large for
    non-base refresh rates eg. 60Hz on a 90Hz display.
    
    This isn't an issue for X11/typical compositors as their present slop
    is 1/2th of the refresh interval so the issue never manifests.
    
    However in Gamescope, the present slop very small and tuned to be
    optimal in real-time to try and reduce display latency significantly.
    This results in us queueing up the atomic commit inside the vertical
    porch region which, due to legacy X11/sync control reasons, means that
    AMDGPU must target the next vblank.
    
    This patch changes that behaviour to make FRR displays match what occurs
    on VRR/Freesync displays where the vertical porch time is not included
    in determining what vblank to target and solves the issue.
    
    This means that smarter compositors can get large input latency
    reductions when using OLED displays at lower than base refresh rates.
    
    For upstreaming this patch, it will need to be considered what the best
    solution is to enable this behaviour from the userspace side.
    Obviously the X11/legacy stuff probably cannot change here -- so we
    either need to enable this new behaviour globally for all DRM atomic
    clients (ie. basically Wayland compositors) or have a
    new DRM_MODE_ATOMIC flag.
    
    Signed-off-by: Joshua Ashton <joshua@froggi.es>
    [Reworked for 6.18 to rebase easier going forward and make it more
     obvious what needs to be done to upstream]
    Signed-off-by: Robert Beckett <bob.beckett@collabora.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -10343,6 +10343,13 @@ static void amdgpu_dm_commit_planes(struct drm_atomic_commit *state,
 		struct dc_stream_update stream_update;
 	} *bundle;
 
+	/*
+	 * Hack: Make unconditional.
+	 * TODO: rework with flag to gate this or keep in in active period
+	 * based on userland selection.
+	 */
+	vrr_active = true;
+
 	bundle = kzalloc_obj(*bundle);
 
 	if (!bundle) {
