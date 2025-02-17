8f7b38da6a4e3c3236ccc4726555f6d101373c04
Alexander Deucher <alexander.deucher@amd.com>
2025-02-17 10:12:49 -0500

    Revert "amdgpu/pm/legacy: fix suspend/resume issues"
    This reverts commit 7e7342563a0a020071f10f280bcda1de0d9f0929.
    
    Reason for revert: Additional changes needed.
    
    Change-Id: Ibc219b8037ccf2021bde1c5407ebc39edc2ca238

--- drivers/gpu/drm/amd/pm/legacy-dpm/kv_dpm.c
+++ drivers/gpu/drm/amd/pm/legacy-dpm/kv_dpm.c
@@ -3042,7 +3042,6 @@ static int kv_dpm_hw_init(struct amdgpu_ip_block *ip_block)
 	if (!amdgpu_dpm)
 		return 0;
 
-	mutex_lock(&adev->pm.mutex);
 	kv_dpm_setup_asic(adev);
 	ret = kv_dpm_enable(adev);
 	if (ret)
@@ -3050,8 +3049,6 @@ static int kv_dpm_hw_init(struct amdgpu_ip_block *ip_block)
 	else
 		adev->pm.dpm_enabled = true;
 	amdgpu_legacy_dpm_compute_clocks(adev);
-	mutex_unlock(&adev->pm.mutex);
-
 	return ret;
 }
 
@@ -3070,39 +3067,31 @@ static int kv_dpm_suspend(struct amdgpu_ip_block *ip_block)
 	struct amdgpu_device *adev = ip_block->adev;
 
 	if (adev->pm.dpm_enabled) {
-		mutex_lock(&adev->pm.mutex);
-		adev->pm.dpm_enabled = false;
 		/* disable dpm */
 		kv_dpm_disable(adev);
 		/* reset the power state */
 		adev->pm.dpm.current_ps = adev->pm.dpm.requested_ps = adev->pm.dpm.boot_ps;
-		mutex_unlock(&adev->pm.mutex);
 	}
 	return 0;
 }
 
 static int kv_dpm_resume(struct amdgpu_ip_block *ip_block)
 {
-	int ret = 0;
+	int ret;
 	struct amdgpu_device *adev = ip_block->adev;
 
-	if (!amdgpu_dpm)
-		return 0;
-
-	if (!adev->pm.dpm_enabled) {
-		mutex_lock(&adev->pm.mutex);
+	if (adev->pm.dpm_enabled) {
 		/* asic init will reset to the boot state */
 		kv_dpm_setup_asic(adev);
 		ret = kv_dpm_enable(adev);
 		if (ret)
 			adev->pm.dpm_enabled = false;
-		else {
+		else
 			adev->pm.dpm_enabled = true;
+		if (adev->pm.dpm_enabled)
 			amdgpu_legacy_dpm_compute_clocks(adev);
-		}
-		mutex_unlock(&adev->pm.mutex);
 	}
-	return ret;
+	return 0;
 }
 
 static bool kv_dpm_is_idle(void *handle)
--- drivers/gpu/drm/amd/pm/legacy-dpm/si_dpm.c
+++ drivers/gpu/drm/amd/pm/legacy-dpm/si_dpm.c
@@ -7786,7 +7786,6 @@ static int si_dpm_hw_init(struct amdgpu_ip_block *ip_block)
 	if (!amdgpu_dpm)
 		return 0;
 
-	mutex_lock(&adev->pm.mutex);
 	si_dpm_setup_asic(adev);
 	ret = si_dpm_enable(adev);
 	if (ret)
@@ -7794,7 +7793,6 @@ static int si_dpm_hw_init(struct amdgpu_ip_block *ip_block)
 	else
 		adev->pm.dpm_enabled = true;
 	amdgpu_legacy_dpm_compute_clocks(adev);
-	mutex_unlock(&adev->pm.mutex);
 	return ret;
 }
 
@@ -7813,41 +7811,31 @@ static int si_dpm_suspend(struct amdgpu_ip_block *ip_block)
 	struct amdgpu_device *adev = ip_block->adev;
 
 	if (adev->pm.dpm_enabled) {
-		mutex_lock(&adev->pm.mutex);
-		adev->pm.dpm_enabled = false;
 		/* disable dpm */
 		si_dpm_disable(adev);
 		/* reset the power state */
 		adev->pm.dpm.current_ps = adev->pm.dpm.requested_ps = adev->pm.dpm.boot_ps;
-		mutex_unlock(&adev->pm.mutex);
 	}
-
 	return 0;
 }
 
 static int si_dpm_resume(struct amdgpu_ip_block *ip_block)
 {
-	int ret = 0;
+	int ret;
 	struct amdgpu_device *adev = ip_block->adev;
 
-	if (!amdgpu_dpm)
-		return 0;
-
-	if (!adev->pm.dpm_enabled) {
+	if (adev->pm.dpm_enabled) {
 		/* asic init will reset to the boot state */
-		mutex_lock(&adev->pm.mutex);
 		si_dpm_setup_asic(adev);
 		ret = si_dpm_enable(adev);
 		if (ret)
 			adev->pm.dpm_enabled = false;
-		else {
+		else
 			adev->pm.dpm_enabled = true;
+		if (adev->pm.dpm_enabled)
 			amdgpu_legacy_dpm_compute_clocks(adev);
-		}
-		mutex_unlock(&adev->pm.mutex);
 	}
-
-	return ret;
+	return 0;
 }
 
 static bool si_dpm_is_idle(void *handle)
