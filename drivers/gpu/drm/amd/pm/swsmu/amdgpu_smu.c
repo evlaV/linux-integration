37acbcf80d3cce54002759b31a21ece5f2eb335b
Mario Limonciello <mario.limonciello@amd.com>
2025-10-01 12:13:40 -0500

    [FROM-ML] drm/amd: Drop superfluous call to set_power_limit()
    smu_set_power_limit() will call set_power_limit() if the limit type
    isn't default ppt, but after a bound check will call set_power_limit()
    regardless.
    
    The first callpath is not necessary, remove it and do the bounds check
    for all calls.
    
    Cc: Lijo Lazar <Lijo.Lazar@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Link: https://lore.kernel.org/amd-gfx/20251001171340.561444-1-mario.limonciello@amd.com/
    Signed-off-by: Robert Beckett <bob.beckett@collabora.com>

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -2880,10 +2880,6 @@ static int smu_set_power_limit(void *handle, uint32_t limit_type, uint32_t limit
 	if (!smu->pm_enabled || !smu->adev->pm.dpm_enabled)
 		return -EOPNOTSUPP;
 
-	if (limit_type != SMU_DEFAULT_PPT_LIMIT)
-		if (smu->ppt_funcs->set_power_limit)
-			return smu->ppt_funcs->set_power_limit(smu, limit_type, limit);
-
 	if ((limit > smu->max_power_limit) || (limit < smu->min_power_limit)) {
 		dev_err(smu->adev->dev,
 			"New power limit (%d) is out of range [%d,%d]\n",
