cd783f5e7f4d3049c195a0d00a91c2349d0bcd2a
Scott Dalton <scottd@valvesoftware.com>
2025-11-21 20:48:02 +0000

    [FOR-UPSTREAM] drm/amdgpu: add Chrontel OUI to freesync pcon whitelist
    Add the OUI for Chrontel.
    I have multiple DP->HDMI devices from different manufacturers that
    report this ID and are confirmed to work.
    
    Signed-off-by: Scott Dalton <scottd@valvesoftware.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
@@ -1560,6 +1560,7 @@ static bool dm_is_freesync_pcon_whitelist(const uint32_t branch_dev_id)
 	case DP_BRANCH_DEVICE_ID_90CC24:
 	case DP_BRANCH_DEVICE_ID_001CF8:
 	case DP_BRANCH_DEVICE_ID_001FF2:
+	case DP_BRANCH_DEVICE_ID_2B02F0:
 		ret_val = true;
 		break;
 	default:
