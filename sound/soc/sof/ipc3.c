f63394eaf30af97484261b0a5995b1d5703ec7f8
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2025-12-11 19:21:35 +0200

    [TEST] ASoC: SOF: ipc3: Do not schedule DSP reset
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- sound/soc/sof/ipc3.c
+++ sound/soc/sof/ipc3.c
@@ -305,7 +305,7 @@ static int ipc3_wait_tx_done(struct snd_sof_ipc *ipc, void *reply_data)
 		ret = -ETIMEDOUT;
 
 		//TODO: add a quirk to conditionally reset DSP
-		schedule_work(&sdev->dsp_reset_work);
+		/* schedule_work(&sdev->dsp_reset_work); */
 	} else {
 		ret = msg->reply_error;
 		if (ret < 0) {
