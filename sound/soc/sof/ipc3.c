f773b8594ac3501b0e95ba0584981e442abf1a15
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2025-12-15 18:46:46 +0200

    [DEBUG] ASoC: SOF: ipc3: Disable ipc rx log header
    Temporary workaround to prevent flooding kernel ring buffer.
    
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- sound/soc/sof/ipc3.c
+++ sound/soc/sof/ipc3.c
@@ -995,7 +995,7 @@ void sof_ipc3_do_rx_work(struct snd_sof_dev *sdev, struct sof_ipc_cmd_hdr *hdr, 
 	u32 cmd;
 	int err;
 
-	ipc3_log_header(sdev->dev, "ipc rx", hdr->cmd);
+	/* ipc3_log_header(sdev->dev, "ipc rx", hdr->cmd); */
 
 	if (hdr->size < sizeof(*hdr) || hdr->size > SOF_IPC_MSG_MAX_SIZE) {
 		dev_err(sdev->dev, "The received message size is invalid: %u\n",
@@ -1048,7 +1048,7 @@ void sof_ipc3_do_rx_work(struct snd_sof_dev *sdev, struct sof_ipc_cmd_hdr *hdr, 
 	/* Notify registered clients */
 	sof_client_ipc_rx_dispatcher(sdev, msg_buf);
 
-	ipc3_log_header(sdev->dev, "ipc rx done", hdr->cmd);
+	/* ipc3_log_header(sdev->dev, "ipc rx done", hdr->cmd); */
 }
 EXPORT_SYMBOL(sof_ipc3_do_rx_work);
 
