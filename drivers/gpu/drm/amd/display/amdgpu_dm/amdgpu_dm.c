1d74478a8128d0cdce9c2f37d58b646851f8ffe7
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-02-16 17:45:07 +0100

    [FROM-ML] drm/amd/display: Restore ALLM support in HDMI vsif
    [Why]
    Support for triggering ALLM in modern TVs is missing.
    
    When HDMI vsif was added in 2019:
    commit 3c2381b92cba ("drm/amd/display: add support for VSIP info packet")
    it was improperly handeled as HDMI actually has two separate vsifs. The
    implementation was based on H14b-vsif and ALLM bit was messing it up
    because H14b-vsif doesn't support ALLM. It was later removed in:
    commit 75f77aafe281 ("drm/amd/display: Send H14b-VSIF specified in HDMI")
    
    ALLM is supported by hf-vsif (HDMI Forum) instead.
    
    [How]
    Add proper logic to construct either h14b-vsif or hf-vsif based on
    required capabilities. Currently, only ALLM from hf-vsif is supported.
    
    Turns out, hf-vsif is almost identical to h14b-vsif, BUT has additional
    two bytes of data after OUI. First byte is static and seems like
    a version supported by leftover define. Second byte consists of 3D and
    ALLM bits.
    
    Implement logic to offset 3D data if building hf-vsif.
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
    Link: https://lore.kernel.org/r/20260216164516.36803-19-tomasz.pakula.oficjalny@gmail.com

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -7805,7 +7805,7 @@ create_stream_for_sink(struct drm_connector *connector,
 
 	if (stream->signal == SIGNAL_TYPE_HDMI_TYPE_A ||
 	    stream->signal == SIGNAL_TYPE_HDMI_FRL)
-		mod_build_hf_vsif_infopacket(stream, &stream->hfvsif_infopacket, false, false);
+		mod_build_hf_vsif_infopacket(stream, &stream->hfvsif_infopacket);
 
 	if (stream->signal == SIGNAL_TYPE_DISPLAY_PORT ||
 	    stream->signal == SIGNAL_TYPE_DISPLAY_PORT_MST ||
