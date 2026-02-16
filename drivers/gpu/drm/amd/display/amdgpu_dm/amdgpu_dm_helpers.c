2ed75805bd8bceeb804b0b54bae9c56fdbc1a995
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-02-16 17:44:58 +0100

    [FROM-ML] drm/amd/display: Refactor PCON VRR compatibility check
    [Why]
    DP->HDMI PCONs prevously entered the DP path
    
    [How]
    Restructure amdgpu_dm_update_freesync_caps() and move
    dm_get_adaptive_sync_support_type() to dm_helpers_is_vrr_pcon_allowed()
    to better reflect what this function does. It never actually gave us any
    other info.
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
    Link: https://lore.kernel.org/r/20260216164516.36803-10-tomasz.pakula.oficjalny@gmail.com

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
@@ -1589,23 +1589,18 @@ static bool dm_is_freesync_pcon_whitelist(const uint32_t branch_dev_id)
 	return ret_val;
 }
 
-enum adaptive_sync_type dm_get_adaptive_sync_support_type(struct dc_link *link)
+bool dm_helpers_is_vrr_pcon_allowed(const struct dc_link *link)
 {
-	struct dpcd_caps *dpcd_caps = &link->dpcd_caps;
-	enum adaptive_sync_type as_type = ADAPTIVE_SYNC_TYPE_NONE;
+	if (link->dpcd_caps.dongle_type != DISPLAY_DONGLE_DP_HDMI_CONVERTER)
+		return false;
 
-	switch (dpcd_caps->dongle_type) {
-	case DISPLAY_DONGLE_DP_HDMI_CONVERTER:
-		if (dpcd_caps->adaptive_sync_caps.dp_adap_sync_caps.bits.ADAPTIVE_SYNC_SDP_SUPPORT == true &&
-			dpcd_caps->allow_invalid_MSA_timing_param == true &&
-			dm_is_freesync_pcon_whitelist(dpcd_caps->branch_dev_id))
-			as_type = FREESYNC_TYPE_PCON_IN_WHITELIST;
-		break;
-	default:
-		break;
-	}
+	if (!link->dpcd_caps.allow_invalid_MSA_timing_param)
+		return false;
 
-	return as_type;
+	if (!link->dpcd_caps.adaptive_sync_caps.dp_adap_sync_caps.bits.ADAPTIVE_SYNC_SDP_SUPPORT)
+		return false;
+
+	return dm_is_freesync_pcon_whitelist(link->dpcd_caps.branch_dev_id);
 }
 
 bool dm_helpers_is_fullscreen(struct dc_context *ctx, struct dc_stream_state *stream)
