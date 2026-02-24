bd2afba86a8c819133316c265274b90490477f2f
Leo Li <sunpeng.li@amd.com>
2026-02-24 13:08:11 -0500

    [FROM-ML] drm/vblank: Add drm_crtc_vblank_is_off() helper
    Add and use an drm internal helper to check if vblanks have been turned
    off (via drm_crtc_vblank_off()) for a CRTC, rather than relying on the
    return value of drm_crtc_vblank_get().
    
    This is in preparation of introducing deferred vblank enable/disable, as
    vblank_get() will not return the expected error code when the driver
    defers vblank enable.
    
    No functional change is intended.
    
    Signed-off-by: Leo Li <sunpeng.li@amd.com>
    Link: https://lore.kernel.org/all/20260224212639.390768-2-sunpeng.li@amd.com/
    Signed-off-by: Matthew Schwartz <matthew.schwartz@linux.dev>

--- drivers/gpu/drm/drm_atomic_helper.c
+++ drivers/gpu/drm/drm_atomic_helper.c
@@ -46,6 +46,7 @@
 
 #include "drm_crtc_helper_internal.h"
 #include "drm_crtc_internal.h"
+#include "drm_internal.h"
 
 /**
  * DOC: overview
@@ -1230,7 +1231,7 @@ disable_outputs(struct drm_device *dev, struct drm_atomic_state *state)
 
 	for_each_oldnew_crtc_in_state(state, crtc, old_crtc_state, new_crtc_state, i) {
 		const struct drm_crtc_helper_funcs *funcs;
-		int ret;
+		bool vblank_off;
 
 		/* Shut down everything that needs a full modeset. */
 		if (!drm_atomic_crtc_needs_modeset(new_crtc_state))
@@ -1258,19 +1259,17 @@ disable_outputs(struct drm_device *dev, struct drm_atomic_state *state)
 		if (!drm_dev_has_vblank(dev))
 			continue;
 
-		ret = drm_crtc_vblank_get(crtc);
+		vblank_off = drm_crtc_vblank_is_off(crtc);
 		/*
 		 * Self-refresh is not a true "disable"; ensure vblank remains
 		 * enabled.
 		 */
 		if (new_crtc_state->self_refresh_active)
-			WARN_ONCE(ret != 0,
+			WARN_ONCE(vblank_off,
 				  "driver disabled vblank in self-refresh\n");
 		else
-			WARN_ONCE(ret != -EINVAL,
+			WARN_ONCE(!vblank_off,
 				  "driver forgot to call drm_crtc_vblank_off()\n");
-		if (ret == 0)
-			drm_crtc_vblank_put(crtc);
 	}
 }
 
