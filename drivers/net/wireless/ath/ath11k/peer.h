de7302aa2b13d8a83e50e4b62a14a7a050249c40
Matthew Leach <matthew.leach@collabora.com>
2026-03-27 14:06:00 +0000

    [FOR-UPSTREAM] ath11k: workaround firmware bug where peer_id=0
    It has been observed that under certain conditions the ath11k firmware
    sets the peer_id=0 for RX'd frames. For standard MPDUs this is fine as
    ath11k_dp_rx_h_find_peer() has a fallback case where it locates the peer
    based upon the source mac address.
    
    However, on an aggregated link, reception of an A-MSDU results in the
    peer not being resolved for the second (any any subsequent) sub-MSDUs.
    This causes the encryption type of the frame to be set to an incorrect
    value, resulting in the sub-MSDUs being dropped by ieee80211.
    
    ath11k_pci 0000:03:00.0: data rx skb 000000002f4b704d len 1534 peer xx:xx:xx:xx:xx:xx 0 ucast sn 3063 he160 rate_idx 9 vht_nss 2 freq 5240 band 1 flag 0x40d1a fcs-err 0 mic-err 0 amsdu-more 0 peer_id 0 first_msdu 1 last_msdu 0
    ath11k_pci 0000:03:00.0: data rx skb 0000000038acd580 len 1534 peer (null) 0 ucast sn 3063 he160 rate_idx 9 vht_nss 2 freq 5240 band 1 flag 0x40d00 fcs-err 0 mic-err 0 amsdu-more 0 peer_id 0 first_msdu 0 last_msdu 1
    
    This patch detects when the firmware has mapped a peer with a peer_id of
    0. In that case, we request an unmap and another mapping of thw same
    peer; we try for a maximum of 3 attempts if the firmware keeps giving us
    a bad ID. At which point we go ahead and use the bad ID as a fallback.
    It has been observed that unmapping an ID of 0 and then remapping the
    same peer seems to coerce firmware into giving us a good ID:
    
    ath11k_pci 0000:03:00.0: dp_htt dp_htt rx msg type :0x4
    ath11k_pci 0000:03:00.0: dp_htt peer unmap vdev 0 peer xx:xx:xx:xx:xx:xx id 7
    ath11k_pci 0000:03:00.0: dp_htt dp_htt rx msg type :0x3
    ath11k_pci 0000:03:00.0: dp_htt peer map vdev 0 peer xx:xx:xx:xx:xx:xx id 0
    <Workaround does unmap and re-map at this point>
    ath11k_pci 0000:03:00.0: dp_htt dp_htt rx msg type :0x4
    ath11k_pci 0000:03:00.0: dp_htt peer unmap vdev 0 peer xx:xx:xx:xx:xx:xx id 0
    ath11k_pci 0000:03:00.0: dp_htt dp_htt rx msg type :0x3
    ath11k_pci 0000:03:00.0: dp_htt peer map vdev 0 peer xx:xx:xx:xx:xx:xx id 1
    
    Signed-off-by: Matthew Leach <matthew.leach@collabora.com>

--- drivers/net/wireless/ath/ath11k/peer.h
+++ drivers/net/wireless/ath/ath11k/peer.h
@@ -47,7 +47,7 @@ struct ath11k_peer *ath11k_peer_find_by_addr(struct ath11k_base *ab,
 					     const u8 *addr);
 struct ath11k_peer *ath11k_peer_find_by_id(struct ath11k_base *ab, int peer_id);
 void ath11k_peer_cleanup(struct ath11k *ar, u32 vdev_id);
-int ath11k_peer_delete(struct ath11k *ar, u32 vdev_id, u8 *addr);
+int ath11k_peer_delete(struct ath11k *ar, u32 vdev_id, const u8 *addr);
 int ath11k_peer_create(struct ath11k *ar, struct ath11k_vif *arvif,
 		       struct ieee80211_sta *sta, struct peer_create_params *param);
 int ath11k_wait_for_peer_delete_done(struct ath11k *ar, u32 vdev_id,
