a54605adc7dcd42776985258cbc187d47ac0d45c
Natalie Vock <natalie.vock@gmx.de>
2026-05-29 00:55:26 +0200

    [FROM-ML] drm/amdgpu: Implement explicit sync for PRT unmaps
    In certain cases, it is possible to skip syncing with every submission
    in the whole VM before unmapping pagetables. The important condition is
    that no memory must be released to the system while the GPU still has a
    chance to access it.
    
    This is true for VM-always-valid buffers since the resv is shared with
    the VM's root PD and thus there will always be synchronization before
    unmapping, but also for PRT pages which do not point to any real memory
    location at all. Bypass implicit sync for unmapping for these buffers if
    userspace requests it. Userspace can request this via a new VM ioctl
    flag.
    
    Signed-off-by: Natalie Vock <natalie.vock@gmx.de>
    
    Link: https://lore.kernel.org/amd-gfx/20260529-prt-explicit-sync-v1-2-9f1c60955cab@gmx.de/T

--- include/uapi/drm/amdgpu_drm.h
+++ include/uapi/drm/amdgpu_drm.h
@@ -844,6 +844,8 @@ struct drm_amdgpu_gem_op {
 #define AMDGPU_VM_MTYPE_RW		(5 << 5)
 /* don't allocate MALL */
 #define AMDGPU_VM_PAGE_NOALLOC		(1 << 9)
+/* Bypass implicit synchronization for VM updates*/
+#define AMDGPU_VM_EXPLICIT_SYNC		(1 << 10)
 
 struct drm_amdgpu_gem_va {
 	/** GEM object handle */
