fa6b424dcb8233b06f3ad75448f0cf65c5e33337
Natalie Vock <natalie.vock@gmx.de>
2026-06-18 17:05:06 +0200

    drm/amdgpu: Invalidate non-per-VM BOs when changing maps too
    We also need to make sure these get updated before they get used again.
    
    Signed-off-by: Natalie Vock <natalie.vock@gmx.de>
    (cherry picked from commit 974ffd3e8da3ff5ccf3975e53cfeccf96e7cbf3d)

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
@@ -1855,6 +1855,8 @@ static void amdgpu_vm_bo_insert_map(struct amdgpu_device *adev,
 
 	if (amdgpu_vm_is_bo_always_valid(vm, bo) && !bo_va->base.moved)
 		amdgpu_vm_bo_moved(&bo_va->base);
+	else if (!bo_va->base.moved)
+		amdgpu_vm_bo_invalidated(&bo_va->base);
 
 	trace_amdgpu_vm_bo_map(bo_va, mapping);
 }
@@ -2157,6 +2159,8 @@ int amdgpu_vm_bo_clear_mappings(struct amdgpu_device *adev,
 		if (amdgpu_vm_is_bo_always_valid(vm, bo) &&
 		    !before->bo_va->base.moved)
 			amdgpu_vm_bo_moved(&before->bo_va->base);
+		else if (!before->bo_va->base.moved)
+			amdgpu_vm_bo_invalidated(&before->bo_va->base);
 	} else {
 		kfree(before);
 	}
@@ -2172,6 +2176,8 @@ int amdgpu_vm_bo_clear_mappings(struct amdgpu_device *adev,
 		if (amdgpu_vm_is_bo_always_valid(vm, bo) &&
 		    !after->bo_va->base.moved)
 			amdgpu_vm_bo_moved(&after->bo_va->base);
+		else if (!after->bo_va->base.moved)
+			amdgpu_vm_bo_invalidated(&after->bo_va->base);
 	} else {
 		kfree(after);
 	}
