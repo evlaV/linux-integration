9f542c74d5f87e92a1981fa1613730130c1a891c
Natalie Vock <natalie.vock@gmx.de>
2026-06-25 15:44:34 +0200

    drm/amdgpu: Only set evicted if the evict isn't soft
    Makes us able to soft-evict already-reserved buffers without
    re-validating them.
    
    Signed-off-by: Natalie Vock <natalie.vock@gmx.de>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
@@ -2411,7 +2411,7 @@ void amdgpu_vm_bo_invalidate(struct amdgpu_bo *bo, bool evicted)
  * Update the memory stats for the new placement and mark @bo as invalid.
  */
 void amdgpu_vm_bo_move(struct amdgpu_bo *bo, struct ttm_resource *new_mem,
-		       bool evicted)
+		       bool evicted, bool soft_evicted)
 {
 	struct amdgpu_vm_bo_base *bo_base;
 
@@ -2424,7 +2424,7 @@ void amdgpu_vm_bo_move(struct amdgpu_bo *bo, struct ttm_resource *new_mem,
 		spin_unlock(&vm->status_lock);
 	}
 
-	amdgpu_vm_bo_invalidate(bo, evicted);
+	amdgpu_vm_bo_invalidate(bo, evicted && !soft_evicted);
 }
 
 /**
