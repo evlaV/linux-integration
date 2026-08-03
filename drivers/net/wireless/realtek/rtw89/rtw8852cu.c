8000c7d1e562db3f4d3690d07e88200a4ec0e735
Nícolas F. R. A. Prado <nfraprado@collabora.com>
2026-08-03 16:25:21 -0400

    Revert "[FROM-UPSTREAM] wifi: rtw89: 8852cu: add quirk to disable 2.4 GHz band"
    This reverts commit 47bc32947958dd80d3900a8e0da44bd7b72864f5.
    
    This commit is causing kernel warnings during scan with the dongle
    connected. It's also causing kernel null deref oops when setting the
    bitrate: 'iw dev wlan1 set bitrates'. Revert for now.
    
    Signed-off-by: Nícolas F. R. A. Prado <nfraprado@collabora.com>

--- drivers/net/wireless/realtek/rtw89/rtw8852cu.c
+++ drivers/net/wireless/realtek/rtw89/rtw8852cu.c
@@ -97,8 +97,7 @@ static const struct rtw89_driver_info rtw89_8852cu_valve_info = {
 	.variant = NULL,
 	.board = &rtw89_8852cu_valve_board,
 	.quirks = NULL,
-	.dev_id_quirks = BIT(RTW89_QUIRK_HW_INFO_SYSFS) |
-			 BIT(RTW89_QUIRK_DISABLE_2GHZ),
+	.dev_id_quirks = BIT(RTW89_QUIRK_HW_INFO_SYSFS),
 	.bus = {
 		.usb = &rtw8852c_usb_info,
 	},
